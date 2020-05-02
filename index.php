e890db6c258f0faee7492d93617082150c53e677
Itzel <ialexia.av@gmail.com>
2020-05-01 18:39:50 -0600

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-<?php echo "Hello World" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
+
+    <!--Uikit JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/css/uikit.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.4.2/dist/js/uikit-icons.min.js"></script>
+
+    <!--jQuery required -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<title>Page title</title>
+</head>
+
+<body>
+
+<div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+        <form class="uk-form-stacked" js-login >
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                </div>
+            </div>
+            
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <button class="uk-button uk-button-default" type="submit">LOGIN</button>
+            </div>
+
+        </form>
+    </div>
+ 
+
+</div>
+
+</body>
+</html>
